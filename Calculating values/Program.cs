fd345380179e7a762bec88437e4213858c9e83f8
vigneshpandi08 <37330800+vigneshpandi08@users.noreply.github.com>
2018-03-16 13:33:23 +0530

    Initial commit

---
+++ Calculating values/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+namespace Calculating_values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Calculation cal=new Calculation();
+        Console.WriteLine("Enter the number1 value:");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the number2 value:");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Addition:"+cal.Addition(n1,n2));
+        Console.WriteLine("Subtraction:"+ cal.Subtraction(n1, n2));
+        Console.WriteLine("Multiplication:"+ cal.Multiplication(n1, n2));
+        Console.WriteLine("Division:"+ cal.Division(n1, n2));
+        Console.ReadLine();
+        }
+    }
+
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Calculation cal = new Calculation();
+            Console.WriteLine("number1 value:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("number2 value:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition:" + cal.Addition(n1, n2));
+            Console.WriteLine("Subtraction:" + cal.Subtraction(n1, n2));
+            Console.WriteLine("Multiplication:" + cal.Multiplication(n1, n2));
+            Console.WriteLine("Division:" + cal.Division(n1, n2));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Calculating values/Program1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyLibrary;
+
+namespace Calculating_values
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Calculation cal = new Calculation();
+            Console.WriteLine("Enter the n1:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the n2:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Addition of two numbers:" + cal.Addition(n1, n2));
+            Console.WriteLine("Subtraction of two numbers:" + cal.Subtraction(n1, n2));
+            Console.WriteLine("Multiplication of two numbwers:" + cal.Multiplication(n1, n2));
+            Console.WriteLine("Division of two numbers:" + cal.Division(n1, n2));
+            Console.ReadLine();
+        }
+    }
+}
